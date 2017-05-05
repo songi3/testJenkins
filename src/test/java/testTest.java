b860db5100363e1da06c33144bd7c47c1b0f3362
songi3 <wks4j1004@gmail.com>
2017-05-06 00:59:20 +0900

    first commit / test jenkins

---
+++ src/test/java/testTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class testTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		test one = new test();
+	}
+
+}
